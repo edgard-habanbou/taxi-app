9e37703fd140a56b1005ba6b2e13e5dcfbe10dd2
MoeKhaweja <mohammadkhawaja1999@gmail.com>
2023-12-11 00:06:55 +0200

    setting up request controller

--- tax-app-backend/app/Http/Controllers/RequestController.php
+++ tax-app-backend/app/Http/Controllers/RequestController.php
@@ -6,5 +6,49 @@ use Illuminate\Http\Request;
 
 class RequestController extends Controller
 {
-    //
+    
+        //get all requests
+        public function index()
+        {
+            $requests = Request::all();
+            return response()->json($requests);
+        }
+        // get request by id
+        public function show($id)
+        {
+            $request = Request::findOrFail($id);
+            return response()->json($request);
+        }
+    
+        public function createRequest(Request $request, $id)
+        {
+            $validatedData = $request->validate([
+                'latitude' => 'required|string',
+                'longitude' => 'required|string',
+                'status' => 'required|integer',
+                'passenger_id' => 'required|integer'.$id
+              ]);
+    
+            $request = Request::create($validatedData);
+    
+            return response()->json($user, 201);
+        }
+    
+        public function updateRequest(Request $request, $id)
+        {
+            $validatedData = $request->validate([
+
+            ]);
+    
+            $request = Request::findOrFail($id);
+
+    
+            $request->update($validatedData);
+    
+            return response()->json($request);
+        }
+    
+
+    
+
 }
