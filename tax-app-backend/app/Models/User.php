a55633e5c1d083fbdceed81c2f40a3a21b06b928
Dina Bader <dina19bader@gmail.com>
2023-12-11 00:13:26 +0200

    added operations to handle rating

--- tax-app-backend/app/Models/User.php
+++ tax-app-backend/app/Models/User.php
@@ -26,6 +26,7 @@ class User extends Authenticatable implements JWTSubject
         'latitude',
         'longitude',
         'image_url',
+        'gender',
 
     ];
 
--- tax-app-backend/routes/api.php
+++ tax-app-backend/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RatingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+});
+
+Route::get('/get_ratings/{user_id}',[RatingController::class,'get_ratings']);
+Route::post('/add_rating',[RatingController::class,'add_rating']);
+Route::delete('/delete_rating/{rating_id}',[RatingController::class,'delete_rating']);
+Route::patch('/update_rating/{rating_id}',[RatingController::class,'update_rating']);
