81e689d4568b0acc54a8491ced03568af1aba6a0
MoeKhaweja <mohammadkhawaja1999@gmail.com>
2023-12-11 12:03:45 +0200

    more work on user request controller

--- tax-app-backend/routes/api.php
+++ tax-app-backend/routes/api.php
@@ -4,7 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\DriverController;
+use App\Http\Controllers\RequestController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -30,4 +31,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/update_location', [RequestController::class, 'updateLocation']);
+Route::post('/update_request_location', [RequestController::class, 'updateLocation']);
+Route::post('/create_request', [RequestController::class, 'createRequest']);
+Route::post('/update_request_status', [RequestController::class, 'updateStatus']);
+Route::post('/update_request_driver', [RequestController::class, 'updateDriver']);
