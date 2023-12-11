2122bb91ba4f5c0e8b2a8dcf72ac2d90b875f584
edgard <edgard.habanbou@hotmail.com>
2023-12-11 11:34:26 +0200

    login page

---
+++ taxi-app-frontend/src/pages/Login/index.jsx
@@ -0,0 +1,69 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsernameChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(
+        "http://localhost:8000/api/login",
+        {
+          email,
+          password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((res) => {
+        if (res.data.authorisation === undefined) {
+          alert("Invalid username or password");
+          setEmail("");
+          setPassword("");
+          return;
+        } else {
+          localStorage.setItem("jwt", res.data.authorisation.token);
+          window.location.href = "/chat";
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input type="text" value={email} onChange={handleUsernameChange} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
