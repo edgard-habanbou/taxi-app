d0fa556d34b9b0b1253119f6294b509d35b31162
edgard <edgard.habanbou@hotmail.com>
2023-12-12 16:55:53 +0200

    Added Login Page Backend

--- taxi-app-admin/src/renderer/src/App.jsx
+++ taxi-app-admin/src/renderer/src/App.jsx
@@ -1,4 +1,5 @@
 import Login from './pages/Login/index'
+import HomePage from './pages/HomePage'
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </div>
   )
--- taxi-app-admin/src/renderer/src/pages/Login/index.jsx
+++ taxi-app-admin/src/renderer/src/pages/Login/index.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
+import axios from 'axios'
 
 import './index.css'
-import TextInput from '../../components/TextInput'
+import TextInput from '../../components/InputText'
 import Button from '../../components/Button'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
-  const loginHandler = (event) => {
-    console.log('login')
-  }
-
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate() // Use useHistory hook
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value)
@@ -20,9 +20,52 @@ const Login = () => {
     setPassword(event.target.value)
   }
 
+  const loginHandler = () => {
+    if (email == '' || password == '') {
+      alert('Please enter your email and password')
+      return
+    } else {
+      setLoading(true)
+      axios
+        .post(
+          'http://localhost:8000/api/login',
+          {
+            email,
+            password
+          },
+          {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          }
+        )
+        .then((res) => {
+          console.log(res)
+          if (res.data.user.role_id == 1) {
+            localStorage.setItem('jwt', res.data.authorisation.token)
+            localStorage.setItem('user', JSON.stringify(res.data.user))
+            navigate('/home')
+          } else {
+            alert('Unauthorized')
+            setEmail('')
+            setPassword('')
+            return
+          }
+        })
+        .catch(() => {
+          alert('Invalid username or password')
+          setEmail('')
+          setPassword('')
+          return
+        })
+        .finally(() => {
+          setLoading(false)
+        })
+    }
+  }
   return (
     <div className="auth">
-      <form onSubmit={loginHandler}>
+      <div className="form">
         <TextInput placeholder="email" value={email} onChange={handleEmailChange} />
 
         <TextInput
@@ -32,11 +75,15 @@ const Login = () => {
           onChange={handlePasswordChange}
         />
         <div className="flex center">
-          <Button onClick={loginHandler} className="btn grey">
-            Login
+          <Button
+            onClick={loginHandler}
+            className={`btn ${loading ? 'grey' : ''}`}
+            disabled={loading}
+          >
+            {loading ? 'Logging in...' : 'Login'}
           </Button>
         </div>
-      </form>
+      </div>
     </div>
   )
 }
